524222b9a426277bd726c869874a012256c617d8
Janindu Dissanayake <janindu001@gmail.com>
2025-09-09 07:20:02 +0000

    { "subject": "mailto: <janindu001@gmail.com>", "publicKey": "BEL-rZ282fl

--- src/app/api/push/send/route.ts
+++ src/app/api/push/send/route.ts
@@ -8,7 +8,7 @@ if (!process.env.VAPID_PRIVATE_KEY || !process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY)
   );
 } else {
   webpush.setVapidDetails(
-    'mailto:your-email@example.com',
+    'mailto:janindu001@gmail.com',
     process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY,
     process.env.VAPID_PRIVATE_KEY
   );
